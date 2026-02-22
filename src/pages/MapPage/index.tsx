31e2ccc004e79e8bb74ac41dbf2dce9c953213a7
TobiasKjernell <tobiaskjernell@gmail.com>
2026-02-22 21:45:10 +0100

    map sharpness

--- src/pages/MapPage/index.tsx
+++ src/pages/MapPage/index.tsx
@@ -225,6 +225,7 @@ const MapPage = () => {
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          detectRetina
         />
 
         <RecenterMap pos={currentPos} />
